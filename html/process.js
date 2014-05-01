2666c1fab69d75b1765114db2064dc7996381630
wangxu <xwang125@xwang125-Latitude-E5410.(none)>
2014-05-01 17:06:36 -0400

    add html files

---
+++ html/process.js
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+        <title>result page</title>
+</head>
+
+
+<script>
+var n = document.getElementById("uniqueName").value;
+
+</script>
+<body>
+
+<h1>Result !</h1>
+
+
+</body>
+</html>
+
